77873d09c9c9a4ae29331f6f4f46a9711f09b37a
yuoo655 <lenuulan@gmail.com>
2020-03-08 16:07:09 +0800

    init

--- main.py
+++ main.py
@@ -6,46 +6,29 @@ from sklearn.model_selection import train_test_split
 from collections import Counter
 import autokeras as ak
 import codecs
-import random
-
 
 
 data = pd.read_csv('train/feats.csv',header=0, sep=',').values
-x_train1 = np.delete(data, (0,4), axis= 1)
-x_train2 = np.delete(data, (1,2,3), axis=1)
-y = x_train2[:,-1]
+x_train = np.delete(data, (0,4), axis= 1)
+y = data[:,-1]
 ids = data[:,0]
-images = []
-labels = []
-x_train = []
-for i in range(len(x_train1)):
-    path = 'train/images/' + ids[i]
-    label = data[i][4]
-    if os.listdir(path) == []:
-        continue
-    j = os.listdir(path)[0]
-    images.append(cv2.resize(cv2.imread(path +'/'+ j, 0), (640,640)))
-    x_train.append(x_train1[i])
-    labels.append(label)
-
-images = np.array(images)
-labels = np.array(labels)
 x_train = np.array(x_train)
-
-
-clf = ak.StructuredDataClassifier(num_classes=4)
-clf.fit(x_train, labels)
+y = np.array(y)
+clf = ak.StructuredDataClassifier(num_classes=4, objective='val_accuracy')
+clf.fit(x_train, y)
 
 
 test_data = pd.read_csv('test/feats.csv',header=0, sep=',').values
-x_test1 = np.delete(test_data, 0, axis= 1)
+x_test = np.delete(test_data, 0, axis= 1)
+predicted_y = clf.predict(x_test)
+predicted_y = predicted_y.reshape((87))
 ids = test_data[:,0]
-y_pred = clf.predict(x_test1)
-y_pred = y_pred.reshape(len(y_pred))
 result = []
-for i in range(len(x_test1)):
-    label = y_pred[i]
+for i in range(len(x_test)):
+    label = predicted_y[i]
     result.append([ids[i], label])
+c = Counter(predicted_y)
+
 with codecs.open('result.csv', 'w', encoding='utf-8') as wf:
     for i in range(len(result)):
         wf.write(result[i][0]+',')
